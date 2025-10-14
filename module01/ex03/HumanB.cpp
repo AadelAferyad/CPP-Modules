18ef334b81731027565881f25b285e1495cc0ef5
aadelAferyad <animevineu@gmail.com>
2025-10-14 14:29:35 +0100

    reference and pointer

---
+++ module01/ex03/HumanB.cpp
@@ -0,0 +1,15 @@
+#include "HumanB.hpp"
+#include <iostream>
+
+HumanB::HumanB(std::string name) : name(name) {}
+
+void	HumanB::attack()
+{
+	std::cout << this->name << " attacks with their ";
+	if (this->weapon)
+		std::cout << this->weapon->getType() << std::endl;
+}
+void	HumanB::setWeapon(Weapon &weapon)
+{
+	this->weapon = &weapon;
+}
