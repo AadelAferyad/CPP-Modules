b51583cf0e54255868a6ffcf55182f30e2231ffd
aadelAferyad <animevineu@gmail.com>
2025-10-16 14:10:39 +0100

    Harl does comlain a lot

---
+++ module01/ex05/Harl.cpp
@@ -0,0 +1,43 @@
+#include "Harl.hpp"
+
+
+void	Harl::debug(void)
+{
+	std::cout << "[ DEBUG ] level: love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "[ INFO ] level: cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "[ WARNING ] level: I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "[ ERROR ] level:  This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	void (Harl::*ptr[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	if (level.empty())
+	{
+		std::cout << "level can't be empty\n";
+		return ;
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		if (level == levels[i])
+		{
+			(this->*ptr[i])();
+			return ;
+		}
+	}
+	std::cout << "Level : " << level << " dosn't exsist\n";
+}
---
+++ module01/ex05/main.cpp
@@ -0,0 +1,17 @@
+#include "Harl.hpp"
+
+
+int	main(void)
+{
+	Harl	harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("");
+	harl.complain("dsad");
+	harl.complain("error");
+	harl.complain("");
+	return (0);
+}
