63305466a6fce8c3c9af0f650431d9556a35b48d
aadelAferyad <animevineu@gmail.com>
2025-12-10 19:56:21 +0100

    ex01

--- module03/ex01/ScavTrap.cpp
+++ module03/ex01/ScavTrap.cpp
@@ -19,8 +19,8 @@ ScavTrap& ScavTrap::operator=(const ScavTrap& other)
 {
 	if (&other != this)
 	{
-		std::cout << "ScavTrap copy assignment operator called" << std::endl;
 		ClapTrap::operator=(other);
+		std::cout << "ScavTrap copy assignment operator called" << std::endl;
 	}
 	return (*this);
 }
@@ -32,13 +32,7 @@ ScavTrap::~ScavTrap()
 
 void	ScavTrap::guardGate()
 {
-    if (this->energy > 0 && this->hit > 0)
-    {
-        this->energy--;
-	std::cout <<  "ScavTrap " << this->name << " is now in Gate keeper mode";
-    }
-    else
-        std::cout << "ScavTrap " << this->name << " has no more Energy Points or Hit Points" << std::endl;
+	std::cout <<  "ScavTrap " << this->name << " is now in Gate keeper mode" << std::endl;
 }
 
 void ScavTrap::attack(const std::string& target)
--- module03/ex01/main.cpp
+++ module03/ex01/main.cpp
@@ -3,6 +3,7 @@
 
 int main() {
     std::cout << "===== Constructors =====" << std::endl;
+    ClapTrap s0;
     ScavTrap s1("Guardian");
     ScavTrap s2(s1);          // Copy constructor
     ScavTrap s3;              // Default constructor
@@ -12,10 +13,10 @@ int main() {
     s1.attack("Intruder");
     s1.takeDamage(20);
     s1.beRepaired(10);
+    s0.attack("this");
 
     std::cout << "\n===== Gate Keeper Mode =====" << std::endl;
     s1.guardGate();
-
     std::cout << "\n===== Energy Test =====" << std::endl;
     for (int i = 0; i < 55; i++) {
         s1.attack("Target");
