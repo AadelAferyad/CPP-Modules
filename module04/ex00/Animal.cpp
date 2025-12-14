0661ab7c2e433af8a45b292946b0ca684eb7167a
aadelAferyad <animevineu@gmail.com>
2025-12-14 20:30:41 +0100

    module 04

---
+++ module04/ex00/Animal.cpp
@@ -0,0 +1,37 @@
+#include "Animal.hpp"
+
+Animal::Animal() 
+{
+	std::cout << "Animal Default constructor called" << std::endl;
+}
+
+Animal::Animal(const std::string &type): type(type)
+{
+	std::cout << "Animal parameterized constructor called" << std::endl;
+}
+
+Animal::Animal(const Animal &other)
+{
+	std::cout << "Animal copy constructor called" << std::endl;
+	this->type = other.type;
+}
+
+Animal Animal::operator=(const Animal &other)
+{
+	std::cout << "Animal copy assignment constructor called" << std::endl;
+	if (this != &other)
+	{
+		this->type = other.type;
+	}
+	return *this;
+}
+
+Animal::~Animal()
+{
+	std::cout << "Animal destructor called" << std::endl;
+}
+
+void	Animal::makeSound() const
+{
+	std::cout << "Animal making sound" << std::endl;
+}
---
+++ module04/ex00/Animal.hpp
@@ -0,0 +1,19 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal
+{
+	protected:
+		std::string	type;
+	public:
+		Animal();
+		Animal(const std::string &type);
+		Animal(const Animal& other);
+		Animal operator=(const Animal &other);
+		virtual ~Animal();
+		void	virtual makeSound() const;
+};
+
+#endif
