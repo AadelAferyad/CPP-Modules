5861db04c0a5d9fca4b0beb76d7a23b6d3589385
AadelAferyad <61366826+AadelAferyad@users.noreply.github.com>
2025-12-29 03:00:37 +0100

    Remove commented-out test code from main.cpp

--- module04/ex03/main.cpp
+++ module04/ex03/main.cpp
@@ -26,59 +26,5 @@ int main(void)
 	    delete me;
 	    delete src;
     }
-    /*std::cout << "General logic Test " << std::endl;*/
-    /*{    */
-    /*    size_t materias_count = 5;*/
-    /*    AMateria *materias[materias_count];*/
-    /*    MateriaSource *src = new MateriaSource();*/
-    /**/
-    /*    Ice *ice = new Ice();*/
-    /*    Cure *cure = new Cure();*/
-    /*    ICharacter *me = new Character("me");*/
-    /*    ICharacter *bob = new Character("bob");*/
-    /*    std::cout << "[ learning materias with src]" << '\n';*/
-    /*    for (size_t i = 0; i < materias_count; i++)*/
-    /*    {*/
-    /*        if (i % 2)*/
-    /*            src->learnMateria(cure);*/
-    /*        else*/
-    /*            src->learnMateria(ice);*/
-    /*    }*/
-    /*    std::cout << "[ Creating materias with src]" << '\n';*/
-    /*    for (size_t i = 0; i < materias_count; i++)*/
-    /*        materias[i] = src->createMateria(i % 2 == 0 ? "ice" : "cure");*/
-    /**/
-    /*    std::cout << "[ Using materias without equiping ]" << '\n';*/
-    /*    for (size_t i = 0; i < materias_count; i++)*/
-    /*        bob->use(i, *me);*/
-    /*    for (size_t i = 0; i < materias_count; i++)*/
-    /*        me->use(i, *bob);*/
-    /**/
-    /*    std::cout << "[ equiping " << materias_count << " materias" << " ]" << '\n';*/
-    /*    for (size_t i = 0; i < materias_count; i++)*/
-    /*    {*/
-    /*        bob->equip(materias[i]);*/
-    /*    }*/
-    /**/
-    /*    std::cout << "[ Using equipment from 0 to " << materias_count << " ]" << '\n';*/
-    /*    for (size_t i = 0; i < materias_count; i++)*/
-    /*    {*/
-    /*        bob->use(i, *me);*/
-    /*    }*/
-    /*    std::cout << "[ unequip from 0 to " << materias_count << " ]" << '\n';*/
-    /*    for (size_t i = 0; i < materias_count; i++)*/
-    /*    {*/
-    /*        bob->unequip(i);*/
-    /*    }*/
-    /*    std::cout << "[Deleting materias] :\n";*/
-    /*    for (size_t i = 0; i < materias_count; i++)*/
-    /*        delete materias[i];*/
-    /**/
-    /*    delete bob;*/
-    /*    delete me;*/
-    /*    delete ice;*/
-    /*    delete cure;*/
-    /*    delete src;*/
-    /*}*/
     return 0;
 }
