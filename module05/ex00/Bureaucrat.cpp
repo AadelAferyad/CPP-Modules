4f54232f5c6df4945b3b8a878ce4508536632cd0
aadelAferyad <animevineu@gmail.com>
2026-02-04 15:19:47 +0100

    module 05

--- module05/ex00/Bureaucrat.cpp
+++ module05/ex00/Bureaucrat.cpp
@@ -1,25 +1,28 @@
 #include "Bureaucrat.hpp"
 
 
-Bureaucrat::Bureaucrat()
+Bureaucrat::Bureaucrat() : name("default"), grade(150)
 {
-	this->name = "default";
-	this->grade = 0;
+	std::cout << "Default constructor callded" << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat &other) 
+Bureaucrat::Bureaucrat(const std::string &name, const int &num) : name(name), grade(num)
 {
-	this->name = other.name;
-	this->grade = other.grade;
+	std::cout << "Parameterized constructor callded" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : name(other.name), grade(other.grade)
+{
+	std::cout << "Copy constructor callded" << std::endl;
 }
 
 Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
 {
 	if (this != &other)
 	{
-		this->name = other.name;
 		this->grade = other.grade;
 	}
+	std::cout << "Copy assignment constructor callded" << std::endl;
 	return (*this);
 }
 
--- module05/ex00/Bureaucrat.hpp
+++ module05/ex00/Bureaucrat.hpp
@@ -2,14 +2,14 @@
 #include <iostream>
 #include <string>
 
-class Bureaucrat:
+class Bureaucrat
 {
 	private:
 		const std::string	name;
 		int	grade;
 	public:
 		Bureaucrat();
-		Bureaucrat(const std::string &name, const int &grade);
+		Bureaucrat(const std::string &name, const int &num);
 		Bureaucrat(const Bureaucrat &other);
 		Bureaucrat &operator=(const Bureaucrat &other);
 		~Bureaucrat();
