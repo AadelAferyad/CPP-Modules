1bfe54ed2f1bef2db409d8b50725f8d466eab9e1
aadelAferyad <animevineu@gmail.com>
2026-06-07 16:50:26 +0100

    ex00

---
+++ module06/ex00/Scalar.cpp
@@ -0,0 +1,9 @@
+#include "Scalar.hpp"
+
+void	ScalarConverter::convert(std::string &str)
+{
+	const char	*s = str.c_str();
+	int	n = std::atoi(s);
+
+}
+
---
+++ module06/ex00/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Scalar.hpp"
+
+
+int	main(void)
+{
+	std::string a = "dsadda";
+	ScalarConverter::convert(a);
+	return (0);
+}
