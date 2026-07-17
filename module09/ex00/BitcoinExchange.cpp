5b99653fa76301b035f5c94cc85e63f8ea5cc14e
aadelAferyad <animevineu@gmail.com>
2026-07-17 12:07:32 +0100

    btc

--- module09/ex00/BitcoinExchange.cpp
+++ module09/ex00/BitcoinExchange.cpp
@@ -38,6 +38,69 @@ std::ostream	&operator<<(std::ostream &out, const Btc &obj)
 	}
 	return (out);
 }
+bool	isValidLeapYear(int year, int month, int day)
+{
+	bool	isLeap = year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+	int	mDay = 0;
+	
+	if (month == 2)
+	{
+		if (isLeap)
+			mDay = 29;
+		else
+			mDay = 28;
+	}
+	else if (month == 4 || month == 6 || month == 9 || month == 11)
+	{
+		mDay = 30;
+	}
+	else
+		mDay = 31;
+	if (day < 1 || day > mDay)
+		return (false);
+	return true;
+}
+
+bool 	checkIsDigit(std::string &str)
+{
+	size_t	i = 0;
+	size_t	l = str.size();
+
+	if (str.empty())
+		return (false);
+	for (; i < l; i++)
+	{
+		if (!std::isdigit(str[i]))
+			return (false);
+	}
+	return (true);
+}
+
+bool	checkAsStr(std::string &date)
+{
+	std::string y;
+	std::string m;
+	std::string d;
+	int	f = 0;
+
+	for (size_t i = 0; i < date.size(); i++)
+	{
+		if (date[i] == '-')
+		{
+			f++;
+			continue ;
+		}
+		if (f == 0)
+			y += date[i];
+		else if (f == 1)
+			m += date[i];
+		else if (f == 2)
+			d += date[i];
+	}
+	if (!checkIsDigit(y) || !checkIsDigit(m) || !checkIsDigit(d))
+		return (false);
+	return true;
+}
 
 void	checkDate(std::string &date)
 {
@@ -48,13 +111,34 @@ void	checkDate(std::string &date)
 	char	sep1;
 
 
-	if (date.size() != 10)
+	if (date.size() != 10 || !checkAsStr(date))
 		throw std::runtime_error("Error: Bad inpute => " + date);
 	std::stringstream stream(date);
 	stream >> year >> sep >> month >> sep1>> day;
-	if (sep != '-' || sep1 != '-' || !year || !month || !(1 <= month && month <= 12) || !day || day > 31)
+	if (sep != '-' || sep1 != '-' || !year || !month || !(1 <= month && month <= 12) || !day || day > 31 || !isValidLeapYear(year, month, day))
 		throw std::runtime_error("Error: Bad inpute => " + date);
-	// check leap year ...	
+}
+
+float	checkValue(std::string &value)
+{
+	int	f = 0;
+	if (value.empty())
+		throw std::runtime_error("Error: Empty value.");
+	std::stringstream	ss(value);
+	float	n;
+	ss >> n;
+	if (n < 0)	
+		throw std::runtime_error("Error: not a positive number.");
+	if (n > 1000)
+		throw std::runtime_error("Error: too large number.");
+	for (size_t i = 0; i < value.size(); i++)
+	{
+		if (value[i] == '.')
+			f++;
+		if (f > 1 || (value[i] != '.' && !std::isdigit(value[i])) || (value[i] == '.' && !std::isdigit(value[i + 1])))
+			throw std::runtime_error("Error: Value should be a digit between 0 and 1000.");
+	}
+	return (n);
 }
 
 void	checkLine(std::map<std::string, float> &data, std::string &line)
@@ -62,18 +146,19 @@ void	checkLine(std::map<std::string, float> &data, std::string &line)
 	std::string	date;
 	std::string	value;
 	char	sep;
+	float	v;
 
-	if (line.empty())
+	if (line.empty() || line.size() < 14)
 		throw std::runtime_error("Error: bad inpute");
 	std::stringstream	stream(line);
 	stream >> date >> sep >> value;
 
-	if (sep != '|')
+	if (sep != '|' || !(line[10] == ' ' && line[11] == '|' && line[12] == ' ' && line[13] != ' '))
 		throw std::runtime_error("Error: bad inpute");
 	checkDate(date);
-	checkValue(value);
+	v = checkValue(value);
 	(void)data;
-	std::cout << date << sep << value << std::endl;
+	std::cout << date << sep << v << std::endl;
 }
 
 void	Btc::loadInpute(char *filename)
